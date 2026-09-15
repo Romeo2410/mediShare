d233c6ac457b4211ea3c6df436866f490a015c74
Rupak <gurrupak24@gmail.com>
2026-09-15 23:18:58 +0530

    Prepare frontend for deployment

--- FrontendMedi/Medicos/src/data/api.js
+++ FrontendMedi/Medicos/src/data/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2003/api"
+  baseURL: "https://medishare-backend-p8tg.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
